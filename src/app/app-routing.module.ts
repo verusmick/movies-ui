c57d2541fc425302edcebd9268e10b23fa636f18
Miguel Angel Angulo Rivera <miguel.angel.angulo72@gmail.com>
2019-02-05 15:13:57 -0400

    Add mockup of: movie list component and movie-card component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MoviesComponent},
+  // {path: 'movie/:id', component: MovieComponent},
+  {path: 'movie', component: MovieComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieCardComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
